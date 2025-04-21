048ecbb318a66bfac1b890d35cc011606c9e91fd
Ricardo1487 <65723408+Ricardo1487@users.noreply.github.com>
2025-04-21 12:24:46 +0200

    Create db_test.py

---
+++ db_test.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+import psycopg2
+
+load_dotenv()  # lädt DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD
+
+try:
+    conn = psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD")
+    )
+    print("✅ Verbindung erfolgreich:", conn.get_dsn_parameters())
+    conn.close()
+except Exception as e:
+    print("❌ Verbindung fehlgeschlagen:", e)
