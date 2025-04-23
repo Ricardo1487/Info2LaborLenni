7e2d27843ad5a104fe4294da895a0057cb9568ec
Ricardo1487 <65723408+Ricardo1487@users.noreply.github.com>
2025-04-23 11:49:14 +0200

    testing .env

--- db_test.py
+++ db_test.py
@@ -5,12 +5,14 @@ import psycopg2
 load_dotenv()  # lädt DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD
 
 try:
+    # Verbindungsaufbau (mit SSL)
     conn = psycopg2.connect(
         host=os.getenv("DB_HOST"),
         port=os.getenv("DB_PORT"),
         dbname=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD")
+        password=os.getenv("DB_PASSWORD"),
+        sslmode="require"
     )
     print("✅ Verbindung erfolgreich:", conn.get_dsn_parameters())
     conn.close()
